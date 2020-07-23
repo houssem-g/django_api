58a85e2ba3dfc0453221f8fa89c7cfe591824182
houssem-g <houssem.gharsalli@hotmail.fr>
2020-07-23 11:53:47 +0200

    add crud function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from '@material-ui/core';
-import axios from 'axios'; // new
+// import axios from 'axios'; // new
 import Table from "./components/Table";
 import './App.css'
 import Requests from './requestHandler.js'
@@ -8,20 +8,13 @@ import Requests from './requestHandler.js'
 class App extends Component {
   state = {
     data: [],
-    cord: {id:"1", lon:"12.1", lat:"12.1", altitude:"0", name:"kaab"},
-    id:"1"
+    cord: {id:"1", lon:"12.1", lat:"12.1", altitude:"12", name:"kaab"},
+    id:"9"
   };
 
-  // used to initialise the state
-  constructor(props) {
-    super(props);
-    // c'est similaire au concept d'héritrage en python, le bind permet de partager le context (les data)
-    // de la fonction getTodos et le composant associé à this (lui mm représente le bouton)
-    this.getTodos = this.getTodos.bind(this)
-  }
-  // we setup the request send here
+
   componentDidMount() {
-    this.setState({ data: [], cord: {lon:"12.1", lat:"12.1", altitude:"0", name:"kaab"}, id:"1" })
+    this.setState({ data: [], cord: {id:"1", lon:"12.1", lat:"12.1", altitude:"12", name:"kaab"}, id:"9"})
     // this.getTodos() 
     document.getElementById('divButton')
     .addEventListener('click', this.handleClick.bind(this))
@@ -31,43 +24,38 @@ class App extends Component {
   handleClick(e) {
     let verb = e.target.textContent
     var args = {}
-    args.headers= {}
-    args.headers.Authorization = 'token d34a596df943a1c86a6f5650475b0ff5da56a007'
     args.method= verb
+    args.url = 'http://127.0.0.1:8000/api/'
+    let newObj = new Requests(args)
     switch (verb) {
       case 'POST':
-        args.url = 'http://127.0.0.1:8000/api/'
         args.data = this.state.cord
+        newObj = new Requests(args)
+        newObj.postData().then(() => {
+          window.alert("Success, please refresh page to see your entry")})
         break;
+      
       case 'PUT':
-        args.url = `http://127.0.0.1:8000/api/${this.state.id}`
+        args.url = `http://127.0.0.1:8000/api/${this.state.id}/`
         args.data = this.state.cord
+        newObj = new Requests(args)
+        newObj.putData().then(() => {
+          window.alert("Success, the row "+ this.state.id + " has been updated")})
         break;
       case 'DELETE':
         args.url = `http://127.0.0.1:8000/api/${this.state.id}`
         args.id = this.state.id
+        newObj = new Requests(args)
+        newObj.deleteData().then(() => {
+          window.alert("Success the row "+ args.id +" has been deleted")})
         break;
       default:
-        args.url = 'http://127.0.0.1:8000/api/'
-    }
-    const myObjectGet = new Requests(args)
-    myObjectGet.getData().then((val) => {
-      this.setState({data: val})
-    })
+        newObj = new Requests(args)
+        newObj.getData().then((val) => {this.setState({data: val})})
+      }
+
   }
 
-  // handle request to backend
-  getTodos(options) {
-  // axios agit comme un fetch
-    console.log('before to send request :', options)
-    axios(options
-    ).then(res => {
-      this.setState({ data: res.data || [] });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  }
 
 
   // console.log(this.columns)
@@ -117,3 +105,13 @@ class App extends Component {
 // https://medium.com/@leannezhang/curly-braces-versus-parenthesis-in-reactjs-4d3ffd33128f
 // https://vaadin.com/learn/tutorials/using-web-components-in-react?hss_channel=lcp-52231&utm_campaign=Learning%20Center&utm_medium=social&utm_source=linkedin&utm_content=81570794
 export default App
+
+
+  // used to initialise the state
+  // constructor(props) {
+  //   super(props);
+  //   // c'est similaire au concept d'héritrage en python, le bind permet de partager le context (les data)
+  //   // de la fonction getTodos et le composant associé à this (lui mm représente le bouton)
+  //   // this.getTodos = this.getTodos.bind(this)
+  // }
+  // we setup the request send here
--- frontend/src/requestHandler.js
+++ frontend/src/requestHandler.js
@@ -2,22 +2,48 @@ import axios from 'axios';
 class  Requests {
     constructor(options) {
         this.options = options
+        this.options.headers= {}
+        this.options.headers.Authorization = 'token d34a596df943a1c86a6f5650475b0ff5da56a007'
+        
         this.data = []
     }
-
-    // get dataDb() {
-    //     return this.getData()
-    // }
+    // function used to iport data from backend
     getData = async () =>{
         // axios agit comme un fetch
-        
         await axios(this.options).then(res => {
             this.data= res.data;
         }).catch(err => {
             console.log(err);
         })
-        return this.data
-        
+        return this.data 
+    }
+    // function used to post data to database
+    postData = async () =>{
+        // axios agit comme un fetch
+        await axios(this.options).then(() => {
+        }).catch(err => {
+            console.log(err);
+        })
+        return "Success"
+    }
+    // function used to update data in database
+    putData = async () =>{
+        // axios agit comme un fetch
+        await axios(this.options).then(() => {
+        }).catch(err => {
+            console.log(err);
+        })
+        return "Success"
+    }
+
+    // function used to delete data from database
+    deleteData = async () =>{
+        // axios agit comme un fetch
+        await axios(this.options).then(() => {
+        }).catch(err => {
+            console.log(err);
+        })
+        return "Success"
     }
 
     
