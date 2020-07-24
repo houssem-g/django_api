cbc3aa1594ddea181b59effbc68c25879ac5c2d5
houssem-g <houssem.gharsalli@hotmail.fr>
2020-07-24 18:12:44 +0200

    change fields to  mandatories

--- frontend/src/components/Formulaire.js
+++ frontend/src/components/Formulaire.js
@@ -15,6 +15,7 @@ class Register extends Component {
       name: '',
 
     };
+    
 
   }
 
@@ -22,7 +23,16 @@ class Register extends Component {
     this.props.visibilityForm(false)
   }
   onSendRequest(){
-    this.props.changeData(this.state)
+    const arrayToCheck = []
+    Object.keys(this.state).map ( inputName => {
+      if (document.getElementsByTagName("input")[inputName].hidden === false && document.getElementsByTagName("input")[inputName].value === '') {
+        arrayToCheck.push('error')
+      };
+      return arrayToCheck
+    })
+    arrayToCheck.length > 0 ? window.alert("Please fill all fields before to send a request.") : this.props.changeData(this.state)
+    
+    
   }
   handleSubmit = (event) => {
     event.preventDefault();
@@ -30,6 +40,7 @@ class Register extends Component {
   }
   handleChange = (event) => {
     const { name, value } = event.target;
+
     
     this.setState({[name]: value});
     
@@ -39,32 +50,32 @@ class Register extends Component {
           <div id='wrapper' className='wrapper'>
             <div className='form-wrapper'>
               
-              <form onSubmit={this.handleSubmit} noValidate>
+              <form id='formId' onSubmit={this.handleSubmit} noValidate>
                 
                   <div onSubmit={this.handleSubmit} className='fullName' >
                     <label htmlFor="fullName"  hidden={this.props.visibityID}>Id</label>
-                    <input type='text' name='id' hidden={this.props.visibityID} onChange={this.handleChange}/>
+                    <input type='text' name='id' required hidden={this.props.visibityID} onChange={this.handleChange}/>
                   </div>
                 
                 <div className='blockOne'>
                     <div>
                         <label hidden={this.props.visibityBody}>Longitude</label>
-                        <input name='lon' hidden={this.props.visibityBody} onChange={this.handleChange}/>
+                        <input name='lon' required hidden={this.props.visibityBody} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label hidden={this.props.visibityBody}>Latitude</label>
-                        <input name='lat' hidden={this.props.visibityBody} onChange={this.handleChange}/>
+                        <input name='lat' required hidden={this.props.visibityBody} onChange={this.handleChange}/>
                     </div>
                 </div>
 
                 <div className='blockOne'>
                     <div>
                         <label hidden={this.props.visibityBody}>Altitude</label>
-                        <input name='altitude' hidden={this.props.visibityBody} onChange={this.handleChange}/>
+                        <input name='altitude' required hidden={this.props.visibityBody} onChange={this.handleChange}/>
                     </div>
                     <div>
                         <label hidden={this.props.visibityBody}>Name</label>
-                        <input name='name'  hidden={this.props.visibityBody} onChange={this.handleChange}/>
+                        <input name='name'  required hidden={this.props.visibityBody} onChange={this.handleChange}/>
                     </div>
                 </div>
  
