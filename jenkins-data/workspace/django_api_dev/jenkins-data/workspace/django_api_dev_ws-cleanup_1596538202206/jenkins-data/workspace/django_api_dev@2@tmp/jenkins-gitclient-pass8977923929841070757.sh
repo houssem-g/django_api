8aec8e5ab9bf7ab4485e238fd2d83e6ca236af82
houssem-g <houssem.gharsalli@hotmail.fr>
2020-08-08 13:40:13 +0200

    ok

--- jenkins-data/workspace/django_api_dev/jenkins-data/workspace/django_api_dev_ws-cleanup_1596538202206/jenkins-data/workspace/django_api_dev@2@tmp/jenkins-gitclient-pass8977923929841070757.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-case "$1" in
-Username*) cat '/var/jenkins_home/workspace/django_api_dev@2@tmp/jenkins-gitclient-username2071490325366559658.txt' ;;
-Password*) cat '/var/jenkins_home/workspace/django_api_dev@2@tmp/jenkins-gitclient-password981567284310883758.txt' ;;
-esac
